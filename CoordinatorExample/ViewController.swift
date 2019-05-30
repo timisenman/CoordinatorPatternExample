89fa46ed8987cb6a870b9aef50af26601f0da113
Tim Isenman <tim.isenman@gmail.com>
2019-05-30 12:28:42 +0900

    Initial Commit

---
+++ CoordinatorExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorExample
+//
+//  Created by Tim Isenman on 5/30/19.
+//  Copyright © 2019 Timothy Isenman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
