1da99aee0bc149ea50535c6cda66bb455376db54
Tim Isenman <tim.isenman@gmail.com>
2019-05-31 12:49:00 +0900

    Add files for Parent and Child Coordinator Relationships

---
+++ CoordinatorExample/BuyCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  BuyCoordinator.swift
+//  CoordinatorExample
+//
+//  Created by Tim Isenman on 5/30/19.
+//  Copyright © 2019 Timothy Isenman. All rights reserved.
+//
+
+import UIKit
+
+class BuyCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    weak var parentCoordinator: MainCoordinator?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let vc = BuyViewController.instantiate()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+//    func didFinishBuying() {
+//        parentCoordinator?.childDidFinish(self)
+//    }
+}
--- CoordinatorExample/MainCoordinator.swift
+++ CoordinatorExample/MainCoordinator.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MainCoordinator: Coordinator {
+class MainCoordinator: NSObject, Coordinator, UINavigationControllerDelegate {
     
     var childCoordinators = [Coordinator]()
     var navigationController: UINavigationController
@@ -21,17 +21,46 @@ class MainCoordinator: Coordinator {
         let vc = ViewController.instantiate()
         vc.coordinator = self
         navigationController.pushViewController(vc, animated: false)
+        navigationController.delegate = self
     }
     
     func buySubscription() {
-        let vc = BuyViewController.instantiate()
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: true)
+        let child = BuyCoordinator(navigationController: navigationController)
+        childCoordinators.append(child)
+        child.start()
+        child.parentCoordinator = self
     }
     
     func createAccount() {
         let vc = CreateAccountViewController.instantiate()
         vc.coordinator = self
         navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func childDidFinish(_ child: Coordinator?) {
+        for (index, coordinator) in childCoordinators.enumerated() {
+            if coordinator === child {
+                childCoordinators.remove(at: index)
+                break
+            }
+        }
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        // Read the view controller we’re moving from.
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+            return
+        }
+        
+        // Check whether our view controller array already contains that view controller. If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
+        if navigationController.viewControllers.contains(fromViewController) {
+            return
+        }
+        
+        // We’re popping the view controller, so we can check whether it’s a buy view controller
+        if let buyViewController = fromViewController as? BuyViewController {
+            // We're popping a buy view controller; end its coordinator
+            childDidFinish(buyViewController.coordinator)
+        }
     }
 }
